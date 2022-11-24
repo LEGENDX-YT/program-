843082a61f5f9c769d8d091e3115202b91ae47d0
【★༒✞︎©༒SATYA-OP】MR-SATYA-OP <104726886+SATYA-OP@users.noreply.github.com>
2022-11-24 06:07:14 -0800

    Create progam.py

---
+++ progam.py
@@ -0,0 +1,10 @@
+from import legendX
+       FROM GIT clone ():
+           try)(_legend_X 
+                     from legend def()"program-spotify"))>
+                       legend = legend("legend_hacker)pro"
+                             try((((:import modules.legend 
+                          install program legendbin.Anon
+                                    anonymous.legend.program.inmstall
+  import config.py
+         from git(git).install def program::/legend
